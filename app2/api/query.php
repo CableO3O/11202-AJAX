941059442e28d5591040c5c61ceb28de85b3a2ac
cable <cable870526@gmail.com>
2023-12-22 14:44:13 +0800

    使用ajax製作資料管理系統

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,15 @@
 <?php
+include_once "./db.php";
 //處理查詢資料的請求
-
+switch ($_GET['do']) {
+    case 'all':
+        header('Content-Type: applocation/json; chaset=utf-8');
+        echo json_encode($Student->all());
+        break;
+    
+    default:
+        # code...
+        break;
+}
 
 ?>
