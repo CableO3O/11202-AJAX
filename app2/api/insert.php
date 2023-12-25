b287598602ef0be04c79453622a2e6486cb172b7
cable <cable870526@gmail.com>
2023-12-25 13:24:53 +0800

    利用ajax載入新增、編輯功能

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,9 +1,6 @@
 <?php
 //處理新增資料的請求
 include_once "db.php";
-
 $Student->save($_POST);
-
 to("../index.html");
-
 ?>
--- app2/api/user_form.php
+++ app2/api/user_form.php
@@ -3,46 +3,55 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="studentformlLabel">新增學生</h1>
+                <h1 class="modal-title fs-5" id="studentformlLabel">
+                    <?=(isset($_GET['id']))?'編輯學生':'新增學生';?>
+                </h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="./api/insert.php" method="post" class="row">
+                <?php 
+                if (isset($_GET['id'])) {
+                    include_once "db.php";
+                        $user=$Student->find($_GET['id']);
+                        extract($user);
+                }
+                ?>
+                <form action="./api/save.php" method="post" class="row">
                     <div class="mb-3 col-6">
                         <label for="name" class="form-label">姓名</label>
-                        <input type="text" class="form-control" name="name" id="name" placeholder="">
+                        <input type="text" class="form-control" name="name" id="name" placeholder="" value="<?=$name??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="school_num" class="form-label">學號</label>
-                        <input type="text" class="form-control" name="school_num" id="school_num" placeholder="">
+                        <input type="text" class="form-control" name="school_num" id="school_num" placeholder="" value="<?=$school_num??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="birthday" class="form-label">生日</label>
-                        <input type="date" class="form-control" name="birthday" id="birthday" placeholder="">
+                        <input type="date" class="form-control" name="birthday" id="birthday" placeholder="" value="<?=$birthday??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="uni_id" class="form-label">身分證字號</label>
-                        <input type="text" class="form-control" name="uni_id" id="uni_id" placeholder="">
+                        <input type="text" class="form-control" name="uni_id" id="uni_id" placeholder="" value="<?=$uni_id??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="addr" class="form-label">地址</label>
-                        <input type="text" class="form-control" name="addr" id="addr" placeholder="">
+                        <input type="text" class="form-control" name="addr" id="addr" placeholder="" value="<?=$addr??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="parents" class="form-label">家長</label>
-                        <input type="text" class="form-control" name="parents" id="parents" placeholder="">
+                        <input type="text" class="form-control" name="parents" id="parents" placeholder="" value="<?=$parents??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="tel" class="form-label">電話</label>
-                        <input type="text" class="form-control" name="tel" id="tel" placeholder="">
+                        <input type="text" class="form-control" name="tel" id="tel" placeholder="" value="<?=$tel??'';?>">
                     </div>
                     <div class="mb-3 col-6">
                         <label for="dept" class="form-label">科系</label>
-                        <input type="text" class="form-control" name="dept" id="dept" placeholder="">
+                        <input type="text" class="form-control" name="dept" id="dept" placeholder="" value="<?=$dept??'';?>">
                     </div>
                     <div class='mb-3 col-6'>
                         <label for="schools" class="form-label">畢業國中</label>
-                        <select id="schools" name="graduate_at" class="form-select" aria-label="Default select">
+                        <select id="schools" name="graduate_at" class="form-select" aria-label="Default select" value="<?=$graduate_at??'';?>">
 
                         </select>
                     </div>
@@ -50,8 +59,15 @@
                         <label for="status_code" class="form-label">畢業狀態</label>
                         <input type="text" class="form-control" name="status_code" id="status_code" placeholder="">
                     </div>
+                    <?php
+                        if (isset($_GET['id'])) {
+                           echo "<input type='hidden' name='id' value='$id'>";
+                        }
+                    ?>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
-                    <button type="submit" class="btn btn-primary">確認新增</button>
+                    <button type="submit" class="btn btn-primary">
+                        <?=(isset($_GET['id']))?'確認編輯':'確認新增';?>
+                    </button>
                 </form>
             </div>
             <div class="modal-footer">
